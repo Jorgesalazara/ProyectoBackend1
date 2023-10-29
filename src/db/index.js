ac4b466433ec18597c02de5061d00254e23bdbc2
Jorge <jorgeernestosalazararrieta@gmail.com>
2023-10-28 19:24:39 -0500

    .

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'; 
 
 let connection = null; 
-let connectionString = 'mongodb+srv://jorgeernestosalazararrieta:Jorgesalazar2001mongo@cluster0.5ibanaw.mongodb.net/?retryWrites=true&w=majority';
+let connectionString = 'mongodb+srv://jorgeernestosalazararrieta:YAlwMvF12VDMo2SHmongo@cluster0.5ibanaw.mongodb.net/?retryWrites=true&w=majority';
 export function createConnection() { 
     if (!connection) { 
         connection = mongoose.createConnection(connectionString, {
